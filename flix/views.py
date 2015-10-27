9946aa5501df96a54e4fb8492bf7ddeaa66fcb64
Matt Palen <matthewmpalen@gmail.com>
2015-10-27 18:07:02 -0400

    Added HomeView

--- flix/views.py
+++ flix/views.py
@@ -1,5 +1,16 @@
 # Django
 from django.views.generic import TemplateView
 
+# Local
+from flix.account.models import ViewEvent
+
 class HomepageView(TemplateView):
     template_name = 'homepage.html'
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['view_event'] = ViewEvent.objects.get(pk=1)
+        return context
