3e49627e818c4f8f842352e02478cf896959d947
Matt Palen <matthewmpalen@gmail.com>
2015-10-27 18:01:43 -0400

    Changed to SimpleRouter

--- flix/account/urls.py
+++ flix/account/urls.py
@@ -1,12 +1,9 @@
-# Django
-from django.conf.urls import include, patterns, url
-
 # External
 from rest_framework import routers
 
 # Local
 from .views import ViewerProfileViewSet, ViewEventViewSet
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r'viewer_profiles', ViewerProfileViewSet)
 router.register(r'view_events', ViewEventViewSet)
