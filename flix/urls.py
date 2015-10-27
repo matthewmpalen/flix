e277917e655e232f95c3783279673106437071d9
Matt Palen <matthewmpalen@gmail.com>
2015-10-27 18:06:41 -0400

    Added HomeView

--- flix/urls.py
+++ flix/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 # Local
-from .views import HomepageView
+from .views import HomepageView, HomeView
 
 urlpatterns = [
-    url(r'^$', HomepageView.as_view(), name='home'), 
+    url(r'^$', HomepageView.as_view(), name='homepage'), 
     url(r'^admin/', include(admin.site.urls)), 
     url(r'^api-auth/', include('rest_framework.urls', 
         namespace='rest_framework')), 
     url(r'^api/v1/', include('flix.api.v1.urls')), 
+    url(r'^home/', HomeView.as_view(), name='home')
 ]
